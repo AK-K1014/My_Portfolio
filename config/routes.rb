12215ab915eba5b1a3fb3f47795de1bca0034e43
{KUDO} <{onigirithannsugithann@yahoo.co.jp}>
2023-05-26 15:20:40 +0900

    login_screen commit-1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root "sessions#top"
+  get "/login", to: "sessions#login"
+  get "/top",   to: "sessions#top"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

4d35c1b85dda67756a13f0bd89665e9998c60c5b
{KUDO} <{onigirithannsugithann@yahoo.co.jp}>
2023-05-30 15:05:29 +0900

    レイアウト修正、routesファイルのインデント調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   root "sessions#top"
-  get "/login", to: "sessions#login"
+  get  "/login", to: "sessions#login"
   post "/login", to: "sessions#create"
-  get '/top_loggedln', to: "top_loggedln#new", as: :top_loggedln
+  get  "/top_loggedln", to: "top_loggedln#new", as: :top_loggedln
 end

ad8969f3bcbe1452dc1f20736136b08b52aead95
{KUDO} <{onigirithannsugithann@yahoo.co.jp}>
2023-06-04 17:13:49 +0900

    修正

--- app/controllers/top_loggedln_controller.rb
+++ app/controllers/top_loggedln_controller.rb
@@ -1,10 +1,35 @@
 class TopLoggedlnController < ApplicationController
-  def new
-    @user = current_user
+  def edit
+    print "こんにちは"
   end
 
   def destroy
     log_out
     redirect_to root_url, status: :see_other
+  end
+  
+  def new
+    @user = current_user
+  end
+
+  def create
+    @user = current_user
+    @user.update(user_params)
+    session[:user_id] = @user.id
+    binding.pry
+    redirect_to top_loggedln_url
+  end
+
+  # def update
+  #   @user = current_user
+  #   @user.update(params.require(:user).permit(:deescription, :image))
+  #   redirect_to top_loggedln_path
+  # end
+
+  private
+
+  def user_params
+    # binding.pry
+    params.require(:user).permit(:description, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root "sessions#top"
   get  "/login", to: "sessions#login"
   post "/login", to: "sessions#create"
-  post "/profile_edit", to: "profile_edit#create"
-  patch "/profile_edit", to: "profile_edit#update"
-  get  "/top_loggedln", to: "top_loggedln#new", as: :top_loggedln
-  get  "/profile_edit", to: "profile_edit#edit"
+  post "/top_loggedln", to: "top_loggedln#create"
+  get  "/top_loggedln", to: "top_loggedln#new"
+  get  "/edit", to: "top_loggedln#edit"
+  patch "/edit", to: "top_loggedln#update"
 end
